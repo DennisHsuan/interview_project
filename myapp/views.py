b905430bec1f05edad2fa29ef19bf27171e73159
Dennis <boyhood515@gmail.com>
2023-03-25 21:32:57 +0800

    database link

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import ProductModel
 
 # Create your views here.
+#建立首頁輪播圖片(跟資料庫互通資料)
 def index(request):
     # return HttpResponse("hello world")
+    products = ProductModel.objects.all()
+    productlist = []
+    for i in range(1, 5):
+        product = ProductModel.objects.get(id = i)
+        productlist.append(product)
     return render(request, 'index.html', locals())
